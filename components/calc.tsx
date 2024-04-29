863ed5d40791c9fe2f5877ea1fa8aef2c118cfc3
Odong Martin <odongmartin21@gmail.com>
2024-04-29 21:33:44 +0300

    calc still has some zibs

--- components/calc.tsx
+++ components/calc.tsx
@@ -82,6 +82,10 @@ const calc = () => {
         if(e.currentTarget.id === 'Clear'){
             screenNumbers = ''
             Number = ''
+            total = 0
+            if(screenRef.current !== null){
+                screenRef.current.innerHTML = '0';
+            }
         }
         if(e.currentTarget.id === 'Backspace'){
             screenNumbers = screenNumbers.substring(0, screenNumbers.length-1)
@@ -147,6 +151,10 @@ const calc = () => {
 
         if(e.currentTarget.id === 'Clear'){
             screenArray = []
+            total = 0;
+            if(screenRef.current !== null){
+                screenRef.current.innerHTML = '0';
+            }
         }
 
         if(e.currentTarget.id === 'Backspace'){
@@ -173,8 +181,18 @@ const calc = () => {
         if (e.currentTarget.id === '=') {
             if(Number !== ''){
                 screenArray.push(Number);
-                Number = '';
+                if(screenArray.length > 2){
+                    Number = '';
+                }
+                else {
+                    total = parseFloat(Number);
+                }
+
             }
+
+            // if(screenArray.length < 3 && screenArray[screenArray.length-1] !== '-'){
+            //     total = parseFloat(Number);
+            // }
 
             if (screenArray.length >=3){
                 for (let i = 0; i < screenArray.length; i++){
@@ -224,9 +242,9 @@ const calc = () => {
                     }
                 }
                 
-                    if(screenRef.current !== null){
-                        screenRef.current.innerHTML = total.toString();
-                    }
+                if(screenRef.current !== null){
+                    screenRef.current.innerHTML = total.toString();
+                }
             }
         }
 
@@ -239,27 +257,44 @@ const calc = () => {
 
   return (
     <>
-        <div>
-            <div ref={screenNumbersRef}> </div>
-            <div ref={screenRef}></div>
-            <div id='1' onClick={handleClick}>1</div>
-            <div id='2' onClick={handleClick}>2</div>
-            <div id='3' onClick={handleClick}>3</div>
-            <div id='4' onClick={handleClick}>4</div>
-            <div id='5' onClick={handleClick}>5</div>
-            <div id='6' onClick={handleClick}>6</div>
-            <div id='7' onClick={handleClick}>7</div>
-            <div id='8' onClick={handleClick}>8</div>
-            <div id='9' onClick={handleClick}>9</div>
-            <div id='0' onClick={handleClick}>0</div>
-            <div id='.' onClick={handleClick}>.</div>
-            <div id='+' onClick={handleClick}>+</div>
-            <div id='-' onClick={handleClick}>-</div>
-            <div id='*' onClick={handleClick}>*</div>
-            <div id='/' onClick={handleClick}>/</div>
-            <div id='Backspace' onClick={handleClick}>Backspace</div>
-            <div id='Clear' onClick={handleClick}>Clear</div>
-            <div id='=' onClick={handleClick}>=</div>
+        <div className="flex justify-center items-center ">
+            <div className="bg-slate-600 text-white w-60 h-auto">
+                <div className="h-20 rounded-lg bg-blue-600">
+                    <div className='px-2 font-bold overflow-hidden' ref={screenNumbersRef}>0</div>
+                    <div className='p-2 font-extrabold text-2xl'ref={screenRef}>0</div>
+                </div>
+                <div className="p-4 w-full">
+                    <div className="flex gap-4 mb-2">
+                        <div className="p-2 bg-gray-500 rounded-full w-20 flex justify-center" style={{cursor: 'pointer'}} id='Clear' onClick={handleClick}>Clear</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-36 flex justify-center" style={{cursor: 'pointer'}} id='Backspace' onClick={handleClick}>Backspace</div>
+                        
+                    </div>
+                    <div className="flex gap-4 mb-2">
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='7' onClick={handleClick}>7</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='8' onClick={handleClick}>8</div>
+                        <div  className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='9' onClick={handleClick}>9</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='*' onClick={handleClick}>*</div>
+                    </div>
+                    <div className="flex gap-4 mb-2">
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='4' onClick={handleClick}>4</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='5' onClick={handleClick}>5</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='6' onClick={handleClick}>6</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='-' onClick={handleClick}>-</div>
+                    </div>
+                    <div className="flex gap-4 mb-2">
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='1' onClick={handleClick}>1</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='2' onClick={handleClick}>2</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='3' onClick={handleClick}>3</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='+' onClick={handleClick}>+</div>
+                    </div>
+                    <div className="flex gap-4 ">
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='0' onClick={handleClick}>0</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='.' onClick={handleClick}>.</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='/' onClick={handleClick}>/</div>
+                        <div className="p-2 bg-gray-500 rounded-full w-12 flex justify-center" style={{cursor: 'pointer'}} id='=' onClick={handleClick}>=</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
   )
